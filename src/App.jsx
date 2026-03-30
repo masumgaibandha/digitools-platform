56666b793e35c91f5b0313e222c4f842a0c286af
masumgaibandha <masumgaibandha@gmail.com>
2026-03-30 11:30:22 +0600

    cart button add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Navbar from './components/Navbar/Navbar'
+import Products from './components/Products/Products'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Products></Products>
       
     </>
   )
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="w-7xl mx-auto">
+      <div className="text-center space-y-3">
+        <h1 className="text-5xl font-extrabold"> Digital Tools</h1>
+        <p className="text-[#627382]">
+          Choose from our curated collection of premium digital products
+          designed to boost your productivity and creativity.
+        </p>
+      </div>
+      <div className="flex justify-center items-center py-5">
+        <button className=" btn bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-white rounded-4xl">
+          Products
+        </button>
+        <button className=" btn  rounded-4xl">
+          Carts (2)
+        </button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Products;
